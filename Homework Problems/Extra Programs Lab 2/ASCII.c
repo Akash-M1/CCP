c1b51678dd6921b7135bed280aaff1159f946b57
Akash-M1 <83898316+Akash-M1@users.noreply.github.com>
2021-05-17 22:23:01 +0530

    Delete Homework Problems/Extra Programs Lab 2 directory

--- Homework Problems/Extra Programs Lab 2/ASCII.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-	char ch ;
-	printf("Enter a Character for which you want ASCII value\n");
-	scanf("%c",&ch);
-	printf("ASCII value of the given Character %c is %d",ch,ch );
-	return 0;
-}
--- Homework Problems/Extra Programs Lab 2/onesplace.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-	int digit,num;
-	printf("Enter the digit from which you want One's place of it\n");
-	scanf("%d",&digit);
-	num=digit%10;
-    printf("One's place of the given digit %d is %d\t",digit,num);
-
-}
